321e434b15e58c41a87203135744deeefcd0e330
davidleegriffin <davidleegriffin@yahoo.com>
2021-06-20 23:49:24 -0500

    added map of austin

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import Logout from './Logout';
 import './HomePage.css';
 
@@ -9,20 +10,42 @@ function HomePage(props) {
     let imgSrc = (props.props.imageUrl) ? props.props.imageUrl : "";
     let googleId = (props.props.googleId) ? props.props.googleId : "";
 
+    const containerStyle = {
+        width: '400px',
+        height: '400px'
+      };
+
+      const center = {
+        lat: 30.2672,
+        lng: -97.7431
+      };
+
     return (
         <div className="home__container--main">
             <div className="home__container--profile">
-                <div>
+                <div className="home__container--name">
                     {name}
                 </div>
-                <div>
+                <div className="home__container--email">
                     {email}
                 </div>
-                <div>
-                    <img src={`${imgSrc}`} alt="profile-pic" />
+                <div className="home__container--profile-image">
+                    <img className="home__image--profile" src={`${imgSrc}`} alt="profile-pic" />
                 </div>
             </div>
             <Logout />
+            <LoadScript
+      googleMapsApiKey="AIzaSyBo4e5Xf0Mbt3QWjz6YsLX01tCCVcDjEq0"
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+    </LoadScript>
         </div>
     )
 }
