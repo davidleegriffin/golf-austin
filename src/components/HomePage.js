8582ad63208f99c723d09678e47d992a681230cc
davidleegriffin <davidleegriffin@yahoo.com>
2021-06-21 23:42:50 -0500

    end of day

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -22,77 +22,77 @@ function HomePage(props) {
 
     const center = {
         lat: 30.2672,
-        lng: -97.7431
+        lng: -97.8231
     };
 
     const locations = [
         {
             name: "UT Golf Club",
-            location: { 
-                lat: 30.35645895033929, 
+            location: {
+                lat: 30.35645895033929,
                 lng: -97.89316004412667,
             },
         },
         {
             name: "Austin Country Club",
-            location: { 
-                lat: 30.342506721735976, 
+            location: {
+                lat: 30.342506721735976,
                 lng: -97.79759910777346,
             },
         },
         {
             name: "Spanish Oaks",
-            location: { 
-                lat: 30.301056531418297,  
+            location: {
+                lat: 30.301056531418297,
                 lng: -97.94533494871781,
             },
         },
         {
             name: "Barton Creek Country Club",
-            location: { 
-                lat: 30.270621406552227,  
+            location: {
+                lat: 30.270621406552227,
                 lng: -97.87561323973802,
             },
         },
         {
             name: "Lost Creek Country Club",
-            location: { 
-                lat: 30.275623121157704,   
+            location: {
+                lat: 30.275623121157704,
                 lng: -97.84929439900793,
             },
         },
         {
             name: "Riverside Golf Course",
-            location: { 
-                lat: 30.247224963112888, 
+            location: {
+                lat: 30.247224963112888,
                 lng: -97.70303065407083,
             },
         },
         {
             name: "Hancock Golf Course",
-            location: { 
-                lat: 30.299353681539625,  
+            location: {
+                lat: 30.299353681539625,
                 lng: -97.7243986431805,
             },
         },
         {
             name: "Morris Williams Golf Course",
-            location: { 
-                lat: 30.289426474278685,  
+            location: {
+                lat: 30.289426474278685,
                 lng: -97.6940893185249,
             },
         },
         {
             name: "Grey Rock Golf and Tennis",
-            location: { 
-                lat: 30.183825034720783,  
+            location: {
+                lat: 30.183825034720783,
                 lng: -97.91382625799028,
             },
         },
         {
             name: "Lions Municipal Golf Course",
-            location: { 
-                lat: 30.290315108240506,  
+            location: {
+                lat: 30.290315108240506,
                 lng: -97.77703278644572,
             },
         }
@@ -129,7 +129,7 @@ function HomePage(props) {
                         {
                             locations.map(item => {
                                 return (
-                                    <Marker key={item.name} 
+                                    <Marker key={item.name}
                                         icon={
                                             'https://icons.iconarchive.com/icons/everaldo/crystal-clear/48/App-golf-game-icon.png'
                                         }
@@ -140,7 +140,7 @@ function HomePage(props) {
                             })
                         }
                         {
-                            selected.location && 
+                            selected.location &&
                             (
                                 <InfoWindow
                                 position={selected.location}
