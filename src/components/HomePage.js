240b02a9e1ac7431b33991b85473de995bf8ee86
davidleegriffin <davidleegriffin@yahoo.com>
2021-06-21 20:35:18 -0500

    added multiple markers with infoWindows

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React, {useState} from 'react';
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 import Logout from './Logout';
 import './HomePage.css';
 
 function HomePage(props) {
+    const [ selected, setSelected ] = useState({});
     console.log('homepage props', props.props);
     let name = (props.props.name) ? props.props.name : "";
     let email = (props.props.email) ? props.props.email : "";
     let imgSrc = (props.props.imageUrl) ? props.props.imageUrl : "";
     let googleId = (props.props.googleId) ? props.props.googleId : "";
+
+    const onSelect = item => {
+        setSelected(item);
+    }
 
     const containerStyle = {
         width: '90vmax',
@@ -19,6 +24,44 @@ function HomePage(props) {
         lat: 30.2672,
         lng: -97.7431
     };
+
+    const locations = [
+        {
+        name: "UT Golf Club",
+        location: { 
+            lat: 30.412344994349912,
+            lng: -97.89943256168085,
+        },
+        },
+        {
+        name: "Spanish Oaks",
+        location: { 
+            lat: 30.345999846740344, 
+            lng: -97.94887104494033,
+        },
+        },
+        {
+        name: "Riverside Golf Course",
+        location: { 
+            lat: 30.247224963112888, 
+            lng: -97.70303065407083,
+        },
+        },
+        {
+        name: "Hancock Golf Course",
+        location: { 
+            lat: 30.306523602979013, 
+            lng: -97.72294337379653,
+        },
+        },
+        {
+        name: "Lions Municipal Golf Course",
+        location: { 
+            lat: 30.300002506709184, 
+            lng: -97.77512843238802,
+        },
+        }
+    ];
 
     return (
         <div className="home__container--main">
@@ -48,18 +91,28 @@ function HomePage(props) {
                         center={center}
                         zoom={10}
                     >
-                        <Marker
-                            position={{
-                            lat: 30.412344994349912,
-                            lng: -97.89943256168085,
-                            }}
-                        />
-                        <Marker
-                            position={{
-                            lat: 30.345999846740344, 
-                            lng: -97.94887104494033,
-                            }}
-                        />
+                        {
+                            locations.map(item => {
+                                return (
+                                    <Marker key={item.name} 
+                                        position={item.location}
+                                        onClick={() => onSelect(item)}
+                                    />
+                                )
+                            })
+                        }
+                        {
+                            selected.location && 
+                            (
+                                <InfoWindow
+                                position={selected.location}
+                                clickable={true}
+                                onCloseClick={() => setSelected({})}
+                                >
+                                <div className="home__infoWindow--name">{selected.name}</div>
+                                </InfoWindow>
+                            )
+                        }
                     </GoogleMap>
                 </LoadScript>
             </div>
