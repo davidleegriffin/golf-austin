89805343ee90ca5ab32944b2c67e700fa8d6da1a
davidleegriffin <davidleegriffin@yahoo.com>
2021-06-22 23:01:26 -0500

    finalized stepzen and styles

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,27 +7,28 @@ import './HomePage.css';
 
 function HomePage(props) {
     const [ selected, setSelected ] = useState({});
-    console.log('homepage props', props.props);
+    // console.log('homepage props', props.props);
     let name = (props.props.name) ? props.props.name : "";
     let email = (props.props.email) ? props.props.email : "";
     let imgSrc = (props.props.imageUrl) ? props.props.imageUrl : "";
     let googleId = (props.props.googleId) ? props.props.googleId : "";
 
-    console.log(googleId);
+    console.log('googleId', googleId);
 
     const {
         data,
         loading,
         error
     } = useQuery(GET_GOLF)
-    
+
     const golf = data?.golf;
-    console.log('golf', golf);
-    
+    // console.log('golf', golf);
+
     if (loading) return <p>Almost there...</p>
     if (error) return <p>{error.message}</p>
 
     const onSelect = item => {
+        console.log('item', item);
         setSelected(item);
     }
 
@@ -41,78 +42,11 @@ function HomePage(props) {
         lng: -97.8231
     };
 
-    const locations = [
-        {
-            name: "UT Golf Club",
-            location: {
-                lat: 30.35645895033929,
-                lng: -97.89316004412667,
-            },
-        },
-        {
-            name: "Austin Country Club",
-            location: {
-                lat: 30.342506721735976,
-                lng: -97.79759910777346,
-            },
-        },
-        {
-            name: "Spanish Oaks",
-            location: {
-                lat: 30.301056531418297,
-                lng: -97.94533494871781,
-            },
-        },
-        {
-            name: "Barton Creek Country Club",
-            location: {
-                lat: 30.270621406552227,
-                lng: -97.87561323973802,
-            },
-        },
-        {
-            name: "Lost Creek Country Club",
-            location: {
-                lat: 30.275623121157704,
-                lng: -97.84929439900793,
-            },
-        },
-        {
-            name: "Riverside Golf Course",
-            location: {
-                lat: 30.247224963112888,
-                lng: -97.70303065407083,
-            },
-        },
-        {
-            name: "Hancock Golf Course",
-            location: {
-                lat: 30.299353681539625,
-                lng: -97.7243986431805,
-            },
-        },
-        {
-            name: "Morris Williams Golf Course",
-            location: {
-                lat: 30.289426474278685,
-                lng: -97.6940893185249,
-            },
-        },
-        {
-            name: "Grey Rock Golf and Tennis",
-            location: {
-                lat: 30.183825034720783,
-                lng: -97.91382625799028,
-            },
-        },
-        {
-            name: "Lions Municipal Golf Course",
-            location: {
-                lat: 30.290315108240506,
-                lng: -97.77703278644572,
-            },
-        }
-    ];
+    let locations = [];
+    golf.map(ele => {
+        // console.log('ele', ele);
+        locations.push(ele);
+    });
 
     return (
         <div className="home__container--main">
@@ -140,11 +74,11 @@ function HomePage(props) {
                     <GoogleMap
                         mapContainerStyle={containerStyle}
                         center={center}
-                        zoom={11.5}
+                        zoom={11}
                     >
                         {
-                            golf.map(item => {
-                                console.log('item', item.Name__A);
+                            locations.map(item => {
+                                // console.log('item', item.Name__A);
                                 return (
                                     <Marker key={item.Name__A}
                                         icon={
@@ -157,10 +91,10 @@ function HomePage(props) {
                             })
                         }
                         {
-                            selected.location &&
+                            selected.Name__A &&
                             (
                                 <InfoWindow
-                                position={selected.location}
+                                position={{lat: selected.Latitude__B, lng: selected.Longitude__C}}
                                 clickable={true}
                                 onCloseClick={() => setSelected({})}
                                 >
