bd7e3a7532a3fac0845c3a180756d72ee46b9af2
davidleegriffin <davidleegriffin@yahoo.com>
2021-06-24 11:57:09 -0500

    Detail component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import SplashPage from './components/SplashPage';
+import Detail from './components/Detail';
 import './App.css';
 
 function App() {
  
   return (
     <div className="App">
-      <SplashPage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={SplashPage} />
+          <Route exact path="/detail" component={Detail} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Detail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Detail() {
+    return (
+        <div>
+            <h2>DETAIL</h2>
+        </div>
+    )
+}
+
+export default Detail;
