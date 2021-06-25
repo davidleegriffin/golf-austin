ae27070144b9c0dbac9a48265f985d660be450d8
davidleegriffin <davidleegriffin@yahoo.com>
2021-06-25 12:35:11 -0500

    check6

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloProvider, ApolloClient } from "@apollo/client";
 import { client } from "./utils/client";
 import './index.css';
 import App from './App';
--- src/utils/client.js
+++ src/utils/client.js
@@ -1,4 +1,4 @@
-import { ApolloClient } from 'apollo-boost';
+import { ApolloClient } from 'apollo/client';
 
 const {
   REACT_APP_STEPZEN_API_KEY,
