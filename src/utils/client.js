0917743a4a8d7c656a699cb10b2183a42b173293
davidleegriffin <davidleegriffin@yahoo.com>
2021-06-25 12:41:04 -0500

    check7

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,4 +1,9 @@
-import { ApolloClient } from 'apollo/client';
+import { InMemoryCache } from 'apollo-boost';
+import { 
+    ApolloClient,  
+    createHttpLink,
+    InMemoryCache,
+} from 'apollo/client';
 
 const {
   REACT_APP_STEPZEN_API_KEY,
@@ -6,8 +11,12 @@ const {
 } = process.env
 
 export const client = new ApolloClient({
-  headers: {
+	link: createHttpLink({
+		credentials: "same-origin",
+		headers: {
     Authorization: `Apikey headyflamingomouth::stepzen.net+1000::d603591e9b22ec93bbf3f204f018c484d285b3e4a26cb956a442d3a249d45414`,
   },
-  uri: "https://HeadyFlamingoMouth.stepzen.net/api/winning-stoat/__graphql",
-})
+    uri: "https://HeadyFlamingoMouth.stepzen.net/api/winning-stoat/__graphql",
+  }),
+  cache: new InMemoryCache(),
+});
