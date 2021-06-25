4a75e34f3f655b7a813ac5bb7256f0c07cc30779
davidleegriffin <davidleegriffin@yahoo.com>
2021-06-25 12:22:56 -0500

    check5

--- src/utils/client.js
+++ src/utils/client.js
@@ -7,7 +7,7 @@ const {
 
 export const client = new ApolloClient({
   headers: {
-    Authorization: `Apikey ${REACT_APP_STEPZEN_API_KEY}`,
+    Authorization: `Apikey headyflamingomouth::stepzen.net+1000::d603591e9b22ec93bbf3f204f018c484d285b3e4a26cb956a442d3a249d45414`,
   },
-  uri: REACT_APP_STEPZEN_ENDPOINT,
+  uri: "https://HeadyFlamingoMouth.stepzen.net/api/winning-stoat/__graphql",
 })
